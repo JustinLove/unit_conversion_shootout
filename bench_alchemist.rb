19188b1df2bcd4fd65ac534b342ea3d533f28aaa
Justin Love <git@JustinLove.name>
2011-12-31 09:56:02 -0600

    geo_units

--- bench_alchemist.rb
+++ bench_alchemist.rb
@@ -1,13 +1,15 @@
 require 'benchmark'
 
+SUBJECT = 'alchemist'
+
 samples = Array.new(100) {rand}
 
 Benchmark.bm do |b|
-  b.report('alchemist - load') { require 'alchemist' }
+  b.report(SUBJECT + ' - load') { require SUBJECT }
 end
 
 Benchmark.bm(10) do |b|
-  b.report('alchemist - conversion') do
+  b.report(SUBJECT + ' - conversion') do
     samples.map {|x| x.meters.to.km}
     samples.map {|x| x.km.to.meters}
     samples.map {|x| x.meters.to.miles}
---
+++ bench_geo_units.rb
@@ -0,0 +1,18 @@
+require 'benchmark'
+
+SUBJECT = 'geo_units'
+
+samples = Array.new(100) {rand}
+
+Benchmark.bm do |b|
+  b.report(SUBJECT + ' - load') { require SUBJECT }
+end
+
+Benchmark.bm(10) do |b|
+  b.report(SUBJECT + ' - conversion') do
+    samples.map {|x| GeoUnits.meters_to :kms, x}
+    samples.map {|x| GeoUnits.kms_to :meters, x}
+    samples.map {|x| GeoUnits.meters_to :miles, x}
+    samples.map {|x| GeoUnits.miles_to :meters, x}
+  end
+end
