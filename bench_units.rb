c8771ae7f4d3903bd7fd3c4d2037b6b943878394
Justin Love <git@JustinLove.name>
2011-12-31 12:23:59 -0600

    units

---
+++ bench_units.rb
@@ -0,0 +1,18 @@
+require 'benchmark'
+
+SUBJECT = 'units/standard'
+
+samples = Array.new(100) {rand}
+
+Benchmark.bm(40) do |b|
+  b.report(SUBJECT + ' - load') { require SUBJECT }
+end
+
+Benchmark.bm(40) do |b|
+  b.report(SUBJECT + ' - conversion') do
+    samples.map {|x| x.meters.to_km}
+    samples.map {|x| x.km.to_meters}
+    samples.map {|x| x.meters.to_miles}
+    samples.map {|x| x.miles.to_meters}
+  end
+end
