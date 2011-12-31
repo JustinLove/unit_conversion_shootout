0ff045dd8947e1f9abad92a65c736dbc71b6f1d1
Justin Love <git@JustinLove.name>
2011-12-31 13:17:34 -0600

    uom

---
+++ bench_uom.rb
@@ -0,0 +1,18 @@
+require 'benchmark'
+
+SUBJECT = 'uom'
+
+samples = Array.new(100) {rand}
+
+Benchmark.bm(40) do |b|
+  b.report(SUBJECT + ' - load') { require SUBJECT }
+end
+
+Benchmark.bm(40) do |b|
+  b.report(SUBJECT + ' - conversion') do
+    samples.map {|x| UOM::Measurement.new(:m, x).as(:km).to_f}
+    samples.map {|x| UOM::Measurement.new(:km, x).as(:m).to_f}
+    samples.map {|x| UOM::Measurement.new(:m, x).as(:mi).to_f}
+    samples.map {|x| UOM::Measurement.new(:mi, x).as(:m).to_f}
+  end
+end
